90a2431644832dc30b67377fb90f5bfd05e970a8
vadimberkut <vadimberkut8@gmail.com>
2020-07-03 09:07:35 +0300

    add example

--- 2-client-and-websocket-signaling-server/client/client.js
+++ 2-client-and-websocket-signaling-server/client/client.js
@@ -1,132 +1,212 @@
+'use strict';
 
 // Taken from, but replaced WebSocket tpo Socket.io
 // https://github.com/eugenp/tutorials/tree/master/webrtc/src/main/resources/static
 
-//connecting to our signaling server 
-var socket = io('http://localhost:3000');
-socket.on('connect', function(){
-    console.log('socket.io connect.');
-    console.log("Connected to the signaling server");
-    socket.send({message: 'test from client'});
-    initialize();
-});
-socket.on('event', function(data){
-    console.log('socket.io event.', data);
-});
-socket.on('disconnect', function(){
-    console.log('socket.io disconnect.');
-});
-socket.on('message', function(message){
-    console.log('socket.io message.', message);
-    console.log("Got message", message);
-    let {event, data} = message;
-    switch (event) {
-        // when somebody wants to call us
-        case "offer":
-            handleOffer(data);
-            break;
-        case "answer":
-            handleAnswer(data);
-            break;
-        // when a remote peer sends an ice candidate to us
-        case "candidate":
-            handleCandidate(data);
-            break;
-        default:
-            break;
-    }
-});
+// https://socket.io/docs/emit-cheatsheet/
 
-function send(message) {
-    socket.send(message);
-}
+(async function() {
 
-var peerConnection;
-var dataChannel;
-var input = document.getElementById("messageInput");
-
-function initialize() {
-    var configuration = null;
-
-    peerConnection = new RTCPeerConnection(configuration, {
-        optional : [ {
-            RtpDataChannels : true
-        } ]
+    //connecting to our signaling server 
+    var socket = io('http://localhost:3000');
+    socket.on('connect', async function(){
+        console.log('socket.io connect.');
+        console.log("Connected to the signaling server");
+        // socket.send({message: 'test from client'});
+        await initialize();
     });
-
-    // Setup ice handling
-    peerConnection.onicecandidate = function(event) {
-        console.log('onicecandidate', event);
-        if (event.candidate) {
-            send({
-                event : "candidate",
-                data : event.candidate
-            });
+    socket.on('event', function(data){
+        console.log('socket.io event.', data);
+    });
+    socket.on('disconnect', function(){
+        console.log('socket.io disconnect.');
+    });
+    socket.on('message', function(message){
+        console.log('socket.io message.', message);
+        let {event, data} = message;
+        switch (event) {
+            // when somebody wants to call us
+            case "offer":
+                handleOffer(data);
+                break;
+            case "answer":
+                handleAnswer(data);
+                break;
+            // when a remote peer sends an ice candidate to us
+            case "candidate":
+                handleCandidate(data);
+                break;
+            default:
+                break;
         }
-    };
-
-    // creating data channel
-    dataChannel = peerConnection.createDataChannel("dataChannel", {
-        reliable : true
     });
 
-    dataChannel.onerror = function(error) {
-        console.log("dataChannel Error occured on datachannel:", error);
-    };
+    function send(message) {
+        socket.send(message);
+    }
 
-    // when we receive a message from the other peer, printing it on the console
-    dataChannel.onmessage = function(event) {
-        console.log("dataChannel message:", event.data);
+    let peerConnection;
+    let dataChannel;
+    let input = document.querySelector("#messageInput");
+    let createOfferButtonEl = document.querySelector("#createOfferButton");
+    let sendDataChannelMessageButtonEl = document.querySelector("#sendDataChannelMessageButton");
 
-        let elem = document.querySelector('#dataChannelMessages');
-        let newElem = document.createElement('p');
-        newElem.innerText = event.data;
-        elem.appendChild(newElem);
-    };
+    createOfferButtonEl.addEventListener('click', function(e) {
+        createOffer();
+    });
+    sendDataChannelMessageButtonEl.addEventListener('click', function(e) {
+        sendDataChannelMessage();
+    });
 
-    dataChannel.onclose = function() {
-        console.log("dataChannel is closed");
-    };
-}
 
-function createOffer() {
-    peerConnection.createOffer(function(offer) {
-        send({
-            event : "offer",
-            data : offer
+    async function initialize() {
+        let configuration = null;
+
+        peerConnection = new RTCPeerConnection(configuration, {
+            optional : [{
+                RtpDataChannels : true
+            }],
         });
-        peerConnection.setLocalDescription(offer);
-    }, function(error) {
-        alert("Error creating an offer");
-    });
-}
 
-function handleOffer(offer) {
-    peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+        // Setup ice handling
+        peerConnection.onicecandidate = function(event) {
+            console.log('onicecandidate', event);
+            if (event.candidate) {
+                send({
+                    event : "candidate",
+                    data : event.candidate
+                });
+            }
+        };
+        peerConnection.onicecandidateerror = function(event) {
+            console.error('onicecandidateerror', event);
+        };
+        peerConnection.oniceconnectionstatechange = function(event) {
+            console.log('oniceconnectionstatechange', event);
+        };
+        peerConnection.onicegatheringstatechange = function(event) {
+            console.log('onicegatheringstatechange', event);
+        };
 
-    // create and send an answer to an offer
-    peerConnection.createAnswer(function(answer) {
-        peerConnection.setLocalDescription(answer);
-        send({
-            event : "answer",
-            data : answer
+        // creating data channel
+        dataChannel = peerConnection.createDataChannel("dataChannel", {
+            reliable : true
         });
-    }, function(error) {
-        alert("Error creating an answer");
-    });
 
-};
+        dataChannel.onerror = function(error) {
+            console.log("dataChannel Error occured on datachannel:", error);
+        };
 
-function handleCandidate(candidate) {
-    peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-};
+        // when we receive a message from the other peer, printing it on the console
+        dataChannel.onmessage = function(event) {
+            console.log("dataChannel message:", event.data);
 
-function handleAnswer(answer) {
-    peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
-    console.log("connection established successfully!!");
-};
+            let elem = document.querySelector('#dataChannelMessages');
+            let newElem = document.createElement('p');
+            newElem.innerText = event.data;
+            elem.appendChild(newElem);
+        };
 
-function sendMessage() {
-    dataChannel.send(input.value);
-    input.value = "";
-}
+        dataChannel.onclose = function() {
+            console.log("dataChannel is closed");
+        };
+
+        // setup local streams
+        const constraints = {'video': true, 'audio': true};
+        const localStream = await navigator.mediaDevices.getUserMedia(constraints);
+        const videoElement = document.querySelector('video#localVideo');
+        videoElement.srcObject = localStream;
+
+        let audioTracks = localStream.getAudioTracks();
+        let videoTracks = localStream.getVideoTracks();
+        if (audioTracks.length > 0) {
+            console.log(`Using audio device: ${audioTracks[0].label}`);
+        }
+        if (videoTracks.length > 0) {
+            console.log(`Using video device: ${videoTracks[0].label}`);
+        }
+        let audioTrack = audioTracks[0];
+        let videoTrack = videoTracks[0];
+
+        peerConnection.addTrack(audioTrack, localStream);
+        peerConnection.addTrack(videoTrack, localStream);
+
+        peerConnection.ontrack = function(event /* RTCTrackEvent */) {
+            console.log("ontrack", event);
+            let {streams, tracks} = event;
+            if(event.streams.length === 0) {
+                return;
+            }
+
+            let stream = event.streams[0];
+            let videoElId = `id_${stream.id}`; // prepend with letters to handle case when id starts with number
+
+            // create video element for remote stream
+            let remoteVideosEl = document.querySelector('#remoteVideos');
+            let videoEl = document.querySelector(`video#${videoElId}`);
+            if(!videoEl) {
+                console.log(`adding video for remote streamId ${videoElId}`);
+                videoEl = document.createElement('video');
+                videoEl.setAttribute('id', videoElId);
+                videoEl.setAttribute('autoplay', '');
+                videoEl.setAttribute('playsinline', '');
+                videoEl.setAttribute('controls', 'false');
+                videoEl.muted = true;
+                videoEl.srcObject = stream;
+                remoteVideosEl.appendChild(videoEl);
+            }
+        }
+
+        // create offer
+        createOffer();
+    }
+
+    function createOffer() {
+        const offerOptions = {
+            offerToReceiveAudio: 1,
+            offerToReceiveVideo: 1
+        };
+
+        peerConnection.createOffer(offerOptions).then(function(offer) {
+            send({
+                event : "offer",
+                data : offer
+            });
+            peerConnection.setLocalDescription(offer);
+        }, function(error) {
+            alert("Error creating an offer");
+        });
+    }
+
+    function handleOffer(offer) {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+
+        // create and send an answer to an offer
+        peerConnection.createAnswer(function(answer) {
+            peerConnection.setLocalDescription(answer);
+            send({
+                event : "answer",
+                data : answer
+            });
+        }, function(error) {
+            alert("Error creating an answer");
+        });
+
+    };
+
+    function handleCandidate(candidate) {
+        peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+    };
+
+    function handleAnswer(answer) {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+        console.log("connection established successfully!!");
+    };
+
+    function sendDataChannelMessage() {
+        dataChannel.send(input.value);
+        input.value = "";
+    }
+
+
+})();
--- 2-client-and-websocket-signaling-server/server/index.js
+++ 2-client-and-websocket-signaling-server/server/index.js
@@ -1,9 +1,21 @@
+'use strict';
 
 // https://www.npmjs.com/package/socket.io
+// https://socket.io/docs/emit-cheatsheet/
+var os = require('os');
 const server = require('http').createServer();
 const io = require('socket.io')(server);
+
 io.on('connection', socket => {
     console.log('socket.io connected.');
+
+    // convenience function to log server messages on the client
+    function log() {
+        console.log.apply(console, arguments);
+        var array = ['Message from server:'];
+        array.push.apply(array, arguments);
+        socket.emit('log', array);
+    }
 
     socket.on('event', data => { 
         console.log('socket.io event.', data);
@@ -14,14 +26,54 @@ io.on('connection', socket => {
 
     // handle the event sent with socket.send()
     socket.on('message', (message) => {
-        console.log('message', message);
+        console.log('message', message.event);
         let {event, data} = message;
         
         // send to all except the sender
+        // for a real app, would be room-only (not broadcast)
         socket.broadcast.send(message);
     });
 
-    socket.send({message: 'test from server'});
+    socket.on('createOrJoin', function(room) {
+        log('Received request to create or join room ' + room);
+    
+        var clientsInRoom = io.sockets.adapter.rooms[room];
+        var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+        log('Room ' + room + ' now has ' + numClients + ' client(s)');
+    
+        if (numClients === 0) {
+          socket.join(room);
+          log('Client ID ' + socket.id + ' created room ' + room);
+          socket.emit('roomCreated', room, socket.id);
+
+        } else if (numClients === 1) {
+          log('Client ID ' + socket.id + ' joined room ' + room);
+          io.sockets.in(room).emit('join', room);
+          socket.join(room);
+          socket.emit('rooJoined', room, socket.id);
+          io.sockets.in(room).emit('roomReady');
+
+        } else { // max two clients
+          socket.emit('roomFull', room);
+        }
+      });
+
+      socket.on('ipaddr', function() {
+        var ifaces = os.networkInterfaces();
+        for (var dev in ifaces) {
+          ifaces[dev].forEach(function(details) {
+            if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+              socket.emit('ipaddr', details.address);
+            }
+          });
+        }
+      });
+    
+      socket.on('bye', function(){
+        console.log('received bye');
+      });
+
+    // socket.send({message: 'test from server'});
 });
 
 const port = 3000;
